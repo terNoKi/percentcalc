534e74beafb6d077c062061756816723257dd358
terNoKi <125992891+terNoKi@users.noreply.github.com>
2026-03-17 00:27:49 +0100

    kolejna funkcja dodająca logikę drugiego przycisku

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function obliczProcent() {
     let wynik = (procent/100) * liczba;
     poleWyniku.innerHTML = `Wynik: ${procent}% z ${liczba} to <b>${wynik}</b>`;
 }
+
+function jakiToProcent(){
+    let calosc = parseFloat(document.getElementById('liczba_całość').value);
+    let czesc = parseFloat(document.getElementById('część').value);
+    let poleWyniku = document.getElementById('wynik2');
+
+    if (isNaN(calosc) || isNaN(czesc)){
+        poleWyniku.innerHTML = "<span class='blad'>Błąd: Wpisz liczby w obu polach!</span>";
+        return;
+    }
+
+    if (calosc === 0){
+        poleWyniku.innerHTML = "<span class='bald'>Błąd: Nie dzielimy przez zero</span>"
+        return
+    }
+
+    let wynik = (czesc/calosc) * 100;
+    poleWyniku.innerHTML = `Wynik: ${czesc} to <b>${wynik}%</b> z ${calosc}`;
+}
